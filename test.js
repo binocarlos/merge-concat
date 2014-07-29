6a7b2bf3857f59782250ed4a92b28f0d6035a167
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-29 10:23:17 -0700

    tests failing

--- test.js
+++ test.js
@@ -1,21 +1,33 @@
 var tape = require('tape')
 var merge = require('./')
 
-tape('do a basic merge', function(t){
-	var data = [{
-		a:'red',
-		b:'blue'
-	},{
-		b:'green'
-	}]
+var data = [{
+	a:'red',
+	b:'blue'
+},{
+	b:'green'
+}]
 
+tape('do a basic merge', function(t){
 	var flat = merge(data)
 
 	console.dir(flat)
+
+	t.equal(flat.a, 'red', 'a=red')
+	t.equal(flat.b, 'green', 'b=green')
 
 	t.end()
 })
 
 tape('run a concatenated pipe example', function(t){
+	var flat = merge(data, function(prev, next, field){
+		return prev.charAt(0)=='b' ? prev + ',' + next : next
+	})
+
+	console.dir(flat)
+
+	t.equal(flat.a, 'red', 'a=red')
+	t.equal(flat.b, 'green', 'b=blue,green')
+
 	t.end()
 })
